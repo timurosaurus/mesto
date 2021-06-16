29409223b0dab5d976ba7fb03f481c0f2bfb62f0
Timur Nazarov <timur.nazarov@hotmail.com>
2021-06-15 21:44:10 -0600

    fix: fixed styles and markups

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,34 @@
+//popup and buttons' variables//
 const popupOpenButtonElement = document.querySelector('.profile__edit-btn');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
-
-function togglePopupVisibility() {
-  popupElement.classList.toggle('popup_opened')
-}
-
-popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
-
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
-
-
 let userName = document.querySelector('.profile__user-name');
 let userBio = document.querySelector('.profile__user-description');
 let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__input_name');
-let jobInput = formElement.querySelector('.form__input_bio');
+let nameInput = formElement.querySelector('.form__input_place_name');
+let jobInput = formElement.querySelector('.form__input_place_bio');
 
+//function for opening the popup//
+function openPopup() {
+  popupElement.classList.add('popup_opened')
+}
 
-function formSubmitHandler(evt) {
+//function for closing the popup//
+function closePopup() {
+  popupElement.classList.remove('popup_opened')
+}
+
+//submit function//
+function submitForm(evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userBio.textContent = jobInput.value;
-  togglePopupVisibility();
+  //openPopup();//
+  closePopup();
 }
-
-formElement.addEventListener('submit', formSubmitHandler);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', submitForm);
 
 
 
