c2bb857bac05f41d140a7bf1e29415b7147c124e
Timur Nazarov <timur.nazarov@hotmail.com>
2021-07-04 18:23:31 -0600

    script changed

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,38 @@
 const popupOpenButtonElement = document.querySelector('.profile__edit-btn');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 let userName = document.querySelector('.profile__user-name');
 let userBio = document.querySelector('.profile__user-description');
 let formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('.form__input_place_name');
 let jobInput = formElement.querySelector('.form__input_place_bio');
+
 
 //function for opening the popup//
 function openPopup() {
