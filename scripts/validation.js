ea0c5f54c21837f70a59b02b78f06b8614ed96b3
timurosaurus <timur.nazarov@hotmail.com>
2021-08-20 23:18:40 -0600

    feat: toggle button function added

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,22 +1,43 @@
+//functions for hiding and showing error messages
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  //closest('.popup__box').querySelector('.form__input-error');
   errorElement.textContent = errorMessage;
   errorElement.classList.add('form__input-error_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  //closest('.popup__box').querySelector('.form__input-error');
   errorElement.textContent = '';
   errorElement.classList.remove('form__input-error_active');
 };
+//________________________________________________________________________________________________________________
 
+//function for toggling button state
+
+const toggleButtonState = (inputList, buttonElement) => {
+  const hasInvalidInput = inputList.some(
+    (inputElement) => !inputElement.validity.valid
+  );
+
+  if (hasInvalidInput) {
+    buttonElement.setAttribute('disabled', true);
+    buttonElement.classList.add('form__save-btn_inactive');
+  } else {
+    buttonElement.removeAttribute('disabled');
+    buttonElement.classList.remove('form__save-btn_inactive');
+  }
+};
+
+//fuction for validity check
 const checkInputValidity = (formElement, inputElement) => {
   const isInputInvalid = !inputElement.validity.valid;
   if (isInputInvalid) {
     const errorMessage = inputElement.validationMessage;
-    showInputError(formElement, inputElement, errorMessage)
+    showInputError(formElement, inputElement, errorMessage);
   } else {
-    hideInputError(formElement, inputElement)
+    hideInputError(formElement, inputElement);
   }
 };
 
@@ -27,11 +48,14 @@ const setEventListeners = (formElement) => {
   });
 
   const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__save-btn');
   inputList.forEach(inputElement => {
     inputElement.addEventListener('input', (event) => {
       checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
+  toggleButtonState(inputList, buttonElement);
 };
 
 //validation enabling fuction
